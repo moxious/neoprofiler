2deac4cea5fd62e90a6776ea050b37d6bd10b265
Moxious <moxious@oldhat.org>
2014-05-06 08:34:24 -0400

    initial import

---
+++ src/main/java/org/mitre/neoprofiler/profile/ParameterizedNeoProfile.java
@@ -0,0 +1,11 @@
+package org.mitre.neoprofiler.profile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParameterizedNeoProfile extends NeoProfile {
+	protected Map<String,Object> params = new HashMap<String,Object>();
+	
+	public Object getParameter(String name) { return params.get(name); } 
+	public void setParameter(String name, Object param) { params.put(name,  param); }  
+}
