c4dfd2d68eca5d2b6a6333a3e84a6a1df9352244
Moxious <moxious@oldhat.org>
2014-05-14 07:23:42 -0400

    minor query changes for speedup

--- src/main/java/org/mitre/neoprofiler/profiler/RelationshipsProfiler.java
+++ src/main/java/org/mitre/neoprofiler/profiler/RelationshipsProfiler.java
@@ -6,17 +6,22 @@ import org.mitre.neoprofiler.NeoProfiler;
 import org.mitre.neoprofiler.profile.NeoProfile;
 import org.mitre.neoprofiler.profile.RelationshipsProfile;
 
+/**
+ * Gathers basic summary statistics on relationships, and schedules additional 
+ * RelationshipTypeProfilers to run depending on what is found. 
+ * @author moxious
+ */
 public class RelationshipsProfiler extends QueryRunner implements Profiler {
 	public NeoProfile run(NeoProfiler parent) {
 		RelationshipsProfile p = new RelationshipsProfile();
-		List<Object> relTypes = runQueryMultipleResult(parent, "match n-[r]->m return distinct(type(r)) as relTypes", "relTypes");
+		List<Object> relTypes = runQueryMultipleResult(parent, "start r=relationship(*) return distinct(type(r)) as relTypes", "relTypes");
 		p.addObservation("Available Relationship Types", relTypes);
 
 		for(Object relType : relTypes) { 
 			parent.schedule(new RelationshipTypeProfiler(""+relType));
 		}
 		
-		p.addObservation("Total Relationships", runQuerySingleResult(parent, "match n-[r]->m return count(distinct(r)) as c", "c"));
+		p.addObservation("Total Relationships", runQuerySingleResult(parent, "start r=relationship(*) return count(r) as c", "c"));
 		
 		return p;
 	}
